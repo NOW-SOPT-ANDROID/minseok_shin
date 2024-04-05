3fcb4298a5206b6575462d41629a15cdb7b7a565
t1nm1ksun <minsuk07672@naver.com>
2024-04-05 16:04:32 +0900

    EDIT: mainlayout edit

--- app/src/main/java/com/sopt/now/compose/MainActivity.kt
+++ app/src/main/java/com/sopt/now/compose/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
 
@@ -28,39 +29,37 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
 fun MainScreen(userId: String, userPw: String, userNn: String, userMBTI: String) {
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically
-        ) {
+        Spacer(modifier = Modifier.height(100.dp))
+        Row {
             Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                contentDescription = "Image 1",
-                modifier = Modifier.padding(16.dp).aspectRatio(0.5f)
+                painter = painterResource(id = R.drawable.ic_launcher_foreground), // Replace R.drawable.ic_launcher_foreground with your image resource
+                contentDescription = "프로필 이미지",
             )
             Column {
                 Text(text = "닉네임: $userNn")
-                Text(text = "ID: $userId")
+                Text(text = "ID : $userId")
                 Text(text = "Password: $userPw")
             }
         }
-        Box(
-            modifier = Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground), // Replace R.drawable.ic_launcher_foreground with your image resource
-                contentDescription = "Image 2",
-                modifier = Modifier.aspectRatio(0.5f) //
-            )
-        }
-        Text(text = "MBTI: $userMBTI", modifier = Modifier.padding(top = 16.dp))
+        Spacer(modifier = Modifier.height(20.dp))
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground), // Replace R.drawable.ic_launcher_foreground with your image resource
+            contentDescription = "MBTI 이미지",
+            modifier = Modifier
+                .padding(16.dp)
+                .aspectRatio(16f / 9f)
+        )
+        Text(text = "MBTI: $userMBTI")
+        Text(text = stringResource(id = R.string.))
+
     }
 }
-
 
 
