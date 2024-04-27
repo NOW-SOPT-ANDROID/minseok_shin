3fcf007d6f63d7610e547b2bf9544e411c485eb4
t1nm1ksun <minsuk07672@naver.com>
2024-04-27 16:18:02 +0900

    .

---
+++ app/src/main/java/com/sopt/now/compose/AuthService.kt
@@ -0,0 +1,14 @@
+package com.sopt.now.compose
+
+import com.sopt.now.compose.data.RequestSignUpDto
+import com.sopt.now.compose.data.ResponseSignUpDto
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("member/join")
+    fun signUp(
+        @Body request: RequestSignUpDto,
+    ): Call<ResponseSignUpDto>
+}
